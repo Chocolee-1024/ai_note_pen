523f54420c506c036151ebfb647b47b048e76c86
chocolee1020@gmail.com <eson102000>
2024-03-29 18:18:10 +0800

    init

---
+++ app/src/main/java/com/example/ai_note/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.ai_note;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private MyCanvasView myCanvasView;
+    private Button clear_button;
+
+    private Button go_mark;
+    private Button strokeWidth;
+    private TextView yellow_button;
+    private TextView red_button;
+    private TextView blue_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myCanvasView = findViewById(R.id.myCanvasView);
+
+        yellow_button = findViewById(R.id.yellow_pen);
+        red_button = findViewById(R.id.red_pen);
+        blue_button = findViewById(R.id.blue_pen);
+        clear_button = findViewById(R.id.clear_button);
+        go_mark = findViewById(R.id.go_mark);
+        strokeWidth = findViewById(R.id.Stroke_width_button);
+        go_mark.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MarkActivity.class);
+            startActivity(intent);
+        });
+        strokeWidth.setOnClickListener(v -> {
+            myCanvasView.setStrokeWidth();
+        });
+        clear_button.setOnClickListener(v -> {
+            myCanvasView.clear();
+        });
+        yellow_button.setOnClickListener(v -> {
+            myCanvasView.setColor(0xFFFFFF99);
+        });
+
+        red_button.setOnClickListener(v -> {
+            myCanvasView.setColor(0xFFFF4081);
+        });
+
+        blue_button.setOnClickListener(v -> {
+            myCanvasView.setColor(0xFF7DF9FF);
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/ai_note/MarkActivity.java
@@ -0,0 +1,25 @@
+package com.example.ai_note;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MarkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mark);
+
+        TextView textView = findViewById(R.id.textView);
+
+
+    }
+}
